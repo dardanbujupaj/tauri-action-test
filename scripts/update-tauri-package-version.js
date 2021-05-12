3ef0c437eda9752706fe7625aad56ab487cc5c45
Dardan Bujupaj <d.bujupaj@bluewin.ch>
2021-05-12 02:43:17 +0200

    move tauri version script to js file

---
+++ scripts/update-tauri-package-version.js
@@ -0,0 +1,13 @@
+const { execSync } = require('child_process')
+
+const version = process.argv[2]
+
+const tauriConf = 'src-tauri/tauri.conf.json'
+
+console.log(`Update tauri package version to ${version}`)
+
+// Update electron app
+execSync(`json -I -f ${tauriConf} -e "this.package.version=${version}"`)
+
+// stage file
+execSync(`git add ${tauriConf}`)
